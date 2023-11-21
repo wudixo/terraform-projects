201420d605334494d941423bfba3431cadbe895e
wudixo <124314655+wudixo@users.noreply.github.com>
2023-11-21 17:03:02 +0000

    Update main.tf

--- 02-Input_Output_variables/main.tf
+++ 02-Input_Output_variables/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_vm" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_pair_name}"
-  security_groups = ["default"]
+  security_groups = ["OG"]
   tags = {
    Name = "EC2-VM"
   }
