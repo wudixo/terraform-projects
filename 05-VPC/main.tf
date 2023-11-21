85f58d236d947781db7a805c7b3611290f53420b
wudixo <124314655+wudixo@users.noreply.github.com>
2023-11-21 16:56:02 +0000

    Update main.tf

--- 05-VPC/main.tf
+++ 05-VPC/main.tf
@@ -1,42 +1,42 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "eu-west-2"
 }
 
 
-resource "tls_private_key" "ashokitkey" {
+resource "tls_private_key" "wudixo" {
  algorithm = "RSA"
 }
 
 resource "aws_key_pair" "generated_key" {
- key_name = "ashokitkey"
- public_key = "${tls_private_key.ashokitkey.public_key_openssh}"
+ key_name = "999"
+ public_key = "${tls_private_key.999.public_key_openssh}"
  depends_on = [
-  tls_private_key.ashokitkey
+  tls_private_key.999
  ]
 }
 
 resource "local_file" "key" {
- content = "${tls_private_key.ashokitkey.private_key_pem}"
- filename = "ashokitkey.pem"
+ content = "${tls_private_key.999.private_key_pem}"
+ filename = "999.pem"
  file_permission ="0400"
  depends_on = [
-  tls_private_key.ashokitkey
+  tls_private_key.999
  ]
 }
 
-resource "aws_vpc" "ashokitvpc" {
+resource "aws_vpc" "wudixovpc" {
  cidr_block = "10.0.0.0/16"
  instance_tenancy = "default"
  enable_dns_hostnames = "true" 
  tags = {
-  Name = "ashokitvpc"
+  Name = "wudixovpc"
  }
 }
 
-resource "aws_security_group" "ashokitsg" {
- name = "ashokitsg"
+resource "aws_security_group" "wudixosg" {
+ name = "wudixosg"
  description = "This firewall allows SSH, HTTP and MYSQL"
- vpc_id = "${aws_vpc.ashokitvpc.id}"
+ vpc_id = "${aws_vpc.wudixovpc.id}"
  
  ingress {
   description = "SSH"
@@ -70,14 +70,14 @@ resource "aws_security_group" "ashokitsg" {
  }
  
  tags = {
-  Name = "ashokitsg"
+  Name = "wudixosg"
  }
 }
 
 resource "aws_subnet" "public" {
- vpc_id = "${aws_vpc.ashokitvpc.id}"
+ vpc_id = "${aws_vpc.wudixovpc.id}"
  cidr_block = "10.0.0.0/24"
- availability_zone = "ap-south-1a"
+ availability_zone = "eu-west-2a"
  map_public_ip_on_launch = "true"
  
  tags = {
@@ -85,51 +85,51 @@ resource "aws_subnet" "public" {
  } 
 }
 resource "aws_subnet" "private" {
- vpc_id = "${aws_vpc.ashokitvpc.id}"
+ vpc_id = "${aws_vpc.wudixovpc.id}"
  cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1b"
+ availability_zone = "eu-west-2b"
  
  tags = {
   Name = "my_private_subnet"
  }
 }
 
-resource "aws_internet_gateway" "ashokitigw" {
- vpc_id = "${aws_vpc.ashokitvpc.id}"
+resource "aws_internet_gateway" "wudixoigw" {
+ vpc_id = "${aws_vpc.wudixovpc.id}"
  
  tags = { 
-  Name = "ashokitigw"
+  Name = "wudixoigw"
  }
 }
 
-resource "aws_route_table" "ashokitrt" {
- vpc_id = "${aws_vpc.ashokitvpc.id}"
+resource "aws_route_table" "wudixort" {
+ vpc_id = "${aws_vpc.wudixovpc.id}"
  
  route {
   cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.ashokitigw.id}"
+  gateway_id = "${aws_internet_gateway.wudixoigw.id}"
  }
  
  tags = {
-  Name = "ashokitrt"
+  Name = "wudixort"
  }
 }
 
 resource "aws_route_table_association" "a" {
  subnet_id = "${aws_subnet.public.id}"
- route_table_id = "${aws_route_table.ashokitrt.id}"
+ route_table_id = "${aws_route_table.wudixort.id}"
 }
 
 resource "aws_route_table_association" "b" {
  subnet_id = "${aws_subnet.private.id}"
- route_table_id = "${aws_route_table.ashokitrt.id}"
+ route_table_id = "${aws_route_table.wudixort.id}"
 }
 
 resource "aws_instance" "myserver" {
  ami = "ami-02a2af70a66af6dfb"
  instance_type = "t2.micro"
  key_name = "${aws_key_pair.generated_key.key_name}"
- vpc_security_group_ids = [ "${aws_security_group.ashokitsg.id}" ]
+ vpc_security_group_ids = [ "${aws_security_group.wudixosg.id}" ]
  subnet_id = "${aws_subnet.public.id}"
  
  tags = {
