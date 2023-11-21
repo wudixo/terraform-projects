20e95177178aaba603d80790fb783dc6dfd10dc8
wudixo <124314655+wudixo@users.noreply.github.com>
2023-11-21 17:01:19 +0000

    Update main.tf

--- 06-Terraform_Modules/modules/vpc/main.tf
+++ 06-Terraform_Modules/modules/vpc/main.tf
@@ -1,63 +1,63 @@
-resource "aws_vpc" "ashokit_custom_vpc" {
+resource "aws_vpc" "wudixo_custom_vpc" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "Ashok IT - Custom VPC"
+    Name = "WUDI XO - Custom VPC"
   }
 }
 
-resource "aws_subnet" "ashokit_public_subnet" {
-  vpc_id            = aws_vpc.ashokit_custom_vpc.id
+resource "aws_subnet" "wudixo_public_subnet" {
+  vpc_id            = aws_vpc.wudixo_custom_vpc.id
   cidr_block        = var.public_sub_1_cidr
 
   tags = {
-    Name = "Ashok IT - Public Subnet"
+    Name = "WUDI XO - Public Subnet"
   }
 }
 
-resource "aws_subnet" "ashokit_private_subnet" {
-  vpc_id            = aws_vpc.ashokit_custom_vpc.id
+resource "aws_subnet" "wudixo_private_subnet" {
+  vpc_id            = aws_vpc.wudixo_custom_vpc.id
   cidr_block        = var.private_sub_1_cidr
 
   tags = {
-    Name = "Ashok IT - Private Subnet"
+    Name = "WUDI XO - Private Subnet"
   }
 }
 
-resource "aws_internet_gateway" "ashokit_igw" {
-  vpc_id = aws_vpc.ashokit_custom_vpc.id
+resource "aws_internet_gateway" "wudixo_igw" {
+  vpc_id = aws_vpc.wudixo_custom_vpc.id
 
   tags = {
-    Name = "Ashok IT - Internet Gateway"
+    Name = "WUDI XO - Internet Gateway"
   }
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.ashokit_custom_vpc.id
+  vpc_id = aws_vpc.wudixo_custom_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ashokit_igw.id
+    gateway_id = aws_internet_gateway.wudixo_igw.id
   }
 
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.ashokit_igw.id
+    gateway_id      = aws_internet_gateway.wudixo_igw.id
   }
 
   tags = {
-    Name = "Ashok IT - Route Table"
+    Name = "WUDI XO - Route Table"
   }
 }
 
 resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.ashokit_public_subnet.id
+  subnet_id      = aws_subnet.wudixo_public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "public_sg" {
   name   = "HTTP and SSH"
-  vpc_id = aws_vpc.ashokit_custom_vpc.id
+  vpc_id = aws_vpc.wudixo_custom_vpc.id
 
   ingress {
     from_port   = 80
