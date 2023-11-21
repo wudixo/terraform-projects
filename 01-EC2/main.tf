96a577f6b4ab913423cb8ed2d8f8f0e20c0b67cf
wudixo <124314655+wudixo@users.noreply.github.com>
2023-11-21 17:02:20 +0000

    Update main.tf

--- 01-EC2/main.tf
+++ 01-EC2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "linux-vm"{	
 	ami = "ami-02e94b011299ef128"
 	instance_type = "t2.micro"
-	key_name = "ashokitkeypair"
-	security_groups = ["default"]
+	key_name = "999"
+	security_groups = ["OG"]
 	tags = {
-		Name = "AshokIT-Linux-VM"
+		Name = "WUDI-VM"
 	}
 }
